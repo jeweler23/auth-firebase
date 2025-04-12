64d8da00ec2a8a90ec97fba19c5cb0e4e469e529
jeweler23 <lopatin.lopatin2011@gmail.com>
2025-04-12 14:31:09 +0300

    feat: add check url request

--- src/utils/axios-interseptors.ts
+++ src/utils/axios-interseptors.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 import {useAuthStore} from "@/stores/auth.ts";
 
 axios.interceptors.request.use((config)=>{
+  if (config.url.includes("signUp") || config.url.includes("signInWithPassword")) {
+    return config;
+  }
   const authStore = useAuthStore()
   const params = new URLSearchParams()
   params.append('auth',authStore.userInfo.token)
